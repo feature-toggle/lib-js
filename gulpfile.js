373e7f924373d1b4a453e0d3e32dc061d8218f62
Christophe Faribault <christophefaribault@gmail.com>
2016-09-04 19:51:49 -0400

    Updated bower file, added package.json file, built lib file

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+
+gulp.task('scripts', function() {
+    return gulp.src('featuretoggle.js')
+        .pipe(rename('featuretoggle.min.js'))
+        .pipe(uglify({
+            preserveComments: 'some',
+            outSourceMap: true
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['scripts']);
